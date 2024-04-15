66ae13654ca8a4a67dbd39d9a55ebbdf154bc706
Daniel Malagurti <danielmalagurti@gmail.com>
2024-04-14 21:17:22 -0300

    style: :lipstick: update pdf export config

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.m5p711kvooo"
+    "revision": "0.3d80sd4v8s8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
