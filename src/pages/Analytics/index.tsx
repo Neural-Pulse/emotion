e35a0fb423ae5b0f66ee123fb5f0a6e34951c87c
Daniel Malagurti <danielmalagurti@gmail.com>
2024-04-16 01:24:29 -0300

    chore: :zap: improve firebase querys

--- src/pages/Analytics/index.tsx
+++ src/pages/Analytics/index.tsx
@@ -3,7 +3,10 @@ import { Box } from '@chakra-ui/react';
 import MoodChart from '../../components/LineChart';
 import { auth, db } from '../../utils/Firebase';
 import { collection, query, where, getDocs } from 'firebase/firestore';
-import moment from 'moment';
+import dayjs from 'dayjs'; // Usando Day.js como alternativa ao Moment.js
+
+// Simples cache de dados em memória
+let moodDataCache = {};
 
 const Analytics = () => {
     const [moodData, setMoodData] = useState([]);
@@ -14,23 +17,33 @@ const Analytics = () => {
                 const user = auth.currentUser;
                 if (user) {
                     const userId = user.uid;
+                    // Verifica se os dados já estão no cache
+                    if (moodDataCache[userId]) {
+                        console.log('Retrieving data from cache');
+                        setMoodData(moodDataCache[userId]);
+                        return;
+                    }
+
                     const moodDataQuery = query(collection(db, 'moodData'), where('userId', '==', userId));
                     const querySnapshot = await getDocs(moodDataQuery);
 
                     const moodData = querySnapshot.docs.map((doc) => doc.data());
                     console.log('Mood data retrieved: ', moodData);
                     const sortedMoodData = moodData.sort((a, b) => {
-                        return moment(a.time).valueOf() - moment(b.time).valueOf();
+                        return dayjs(a.time).valueOf() - dayjs(b.time).valueOf();
                     });
 
                     const formattedMoodData = sortedMoodData.map((data) => {
                         const timestamp = data.time;
-                        const formattedTimestamp = moment(timestamp).format('DD/MM HH:mm');
+                        const formattedTimestamp = dayjs(timestamp).format('DD/MM HH:mm');
                         return {
                             ...data,
                             time: formattedTimestamp,
                         };
                     });
+
+                    // Armazena os dados formatados no cache
+                    moodDataCache[userId] = formattedMoodData;
                     setMoodData(formattedMoodData);
                 } else {
                     console.log('User not authenticated');
