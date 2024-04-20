d9cb151b6310ba2e9fd7b2f93d1869c43287c10e
Daniel Malagurti <danielmalagurti@gmail.com>
2024-04-20 15:03:48 -0300

    feat: improve menu functionalities

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,3 +1,4 @@
+// SideBar/index.tsx
 import {
     Drawer,
     DrawerBody,
@@ -5,11 +6,8 @@ import {
     DrawerOverlay,
     DrawerContent,
     DrawerCloseButton,
-    Button,
-    useDisclosure
 } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
-
 
 interface SidebarProps {
     isOpen: boolean;
@@ -17,6 +15,11 @@ interface SidebarProps {
 }
 
 const Sidebar = ({ isOpen, onClose }: SidebarProps) => {
+    const handleClose = () => {
+        console.log("Closing sidebar after navigation.");
+        onClose();
+    };
+
     return (
         <>
             <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
@@ -25,8 +28,8 @@ const Sidebar = ({ isOpen, onClose }: SidebarProps) => {
                     <DrawerCloseButton />
                     <DrawerHeader>Navigation Menu</DrawerHeader>
                     <DrawerBody>
-                        <Link to="/about">About</Link>
-                        {/* Navigation links here */}
+                        <Link to="/about" onClick={handleClose}>About</Link>
+                        {/* Add more navigation links here, each wrapped with onClick={handleClose} */}
                     </DrawerBody>
                 </DrawerContent>
             </Drawer>
